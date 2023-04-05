69736ce05cbd8f25ef55b33a885bb2a47cd355b7
Ryan Hiebert <ryan@ryanhiebert.com>
2023-04-05 15:16:59 +0000

    Redo contacts example with layouts
    Drop the temporary examples

--- temploco/contacts.py
+++ temploco/contacts.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.db.models import Model, CharField, Q
-from django.views import View
+from django.views.decorators.http import require_POST, require_GET, require_http_methods
+from .nested import LayoutResponse, PartialResponse
 
 
 class Contact(Model):
@@ -13,21 +14,26 @@ class Contact(Model):
     email = CharField(max_length=256)
 
 
-def contacts(request: HttpRequest) -> HttpResponse:
+def layout(request: HttpRequest) -> LayoutResponse:
+    return LayoutResponse.render(request, "temploco/layout.html")
+
+
+@require_GET
+def contacts(request: HttpRequest) -> PartialResponse:
     search = request.GET.get("q")
     contacts = Contact.objects.all()
     if search is not None:
         contacts = contacts.filter(
             Q(first__icontains=search) | Q(last__icontains=search)
         )
-    return render(request, "temploco/contacts/index.html", {"contacts": contacts})
+    return PartialResponse.render(
+        request, "temploco/contacts/index.html", {"contacts": contacts}
+    )
 
 
-class New(View):
-    def get(self, request: HttpRequest) -> HttpResponse:
-        return render(request, "temploco/contacts/new.html", {"contact": Contact()})
-
-    def post(self, request: HttpRequest) -> HttpResponse:
+@require_http_methods(["GET", "POST"])
+def new(request: HttpRequest) -> PartialResponse | HttpResponse:
+    if request.method == "POST":
         Contact.objects.create(
             first=request.POST["first_name"],
             last=request.POST["last_name"],
@@ -35,20 +41,22 @@ class New(View):
             email=request.POST["email"],
         )
         return redirect("/contacts/")
+    return PartialResponse.render(
+        request, "temploco/contacts/new.html", {"contact": Contact()}
+    )
 
 
-class Detail(View):
-    def get(self, request: HttpRequest, *, id: int) -> HttpResponse:
-        contact = Contact.objects.get(id=id)
-        return render(request, "temploco/contacts/show.html", {"contact": contact})
+@require_GET
+def detail(request: HttpRequest, *, id: int) -> PartialResponse:
+    contact = Contact.objects.get(id=id)
+    return PartialResponse.render(
+        request, "temploco/contacts/show.html", {"contact": contact}
+    )
 
 
-class Edit(View):
-    def get(self, request: HttpRequest, *, id: int) -> HttpResponse:
-        contact = Contact.objects.get(id=id)
-        return render(request, "temploco/contacts/edit.html", {"contact": contact})
-
-    def post(self, request: HttpRequest, *, id: int) -> HttpResponse:
+@require_http_methods(["GET", "POST"])
+def edit(request: HttpRequest, *, id: int) -> PartialResponse | HttpResponse:
+    if request.method == "POST":
         contact = Contact.objects.get(id=id)
         contact.first = request.POST["first_name"]
         contact.last = request.POST["last_name"]
@@ -56,10 +64,14 @@ class Edit(View):
         contact.email = request.POST["email"]
         contact.save()
         return redirect(f"/contacts/{contact.pk}/")
+    contact = Contact.objects.get(id=id)
+    return PartialResponse.render(
+        request, "temploco/contacts/edit.html", {"contact": contact}
+    )
 
 
-class Delete(View):
-    def post(self, request: HttpRequest, *, id: int) -> HttpResponse:
-        contact = Contact.objects.get(id=id)
-        contact.delete()
-        return redirect("/contacts")
+@require_POST
+def delete(request: HttpRequest, *, id: int) -> HttpResponse:
+    contact = Contact.objects.get(id=id)
+    contact.delete()
+    return redirect("/contacts")
--- temploco/urls.py
+++ temploco/urls.py
@@ -1,71 +1,32 @@
-from django.http import HttpRequest
-from .nested import (
-    Route,
-    LayoutResponse,
-    PartialResponse,
-)
-
+from .nested import Route
 from .index import index
 from . import contacts
-
-
-def spam(request: HttpRequest, spam_id: int):
-    return LayoutResponse.render(
-        request,
-        "temploco/nested/spam.html",
-        {"spam_id": spam_id},
-    )
-
-
-def eggs_x(request: HttpRequest, spam_id: int, eggs_id: int):
-    return PartialResponse.render(
-        request,
-        "temploco/nested/x.html",
-        {"spam_id": spam_id, "eggs_id": eggs_id},
-    )
-
-
-def eggs_y(request: HttpRequest, spam_id: int, eggs_id: int):
-    return PartialResponse.render(
-        request,
-        "temploco/nested/y.html",
-        {"spam_id": spam_id, "eggs_id": eggs_id},
-    )
 
 
 app_name = "temploco"
 urlpatterns = [
     Route(
         path="",
+        layout=contacts.layout,
         children=[
             Route(path="", view=index, name="index"),
-            Route(path="contacts", view=contacts.contacts, name="contacts"),
-            Route(
-                path="contacts/new", view=contacts.New.as_view(), name="contacts-new"
-            ),
+            Route(path="contacts/", view=contacts.contacts, name="contacts"),
+            Route(path="contacts/new", view=contacts.new, name="contacts-new"),
             Route(
                 path="contacts/<int:id>/",
-                view=contacts.Detail.as_view(),
+                view=contacts.detail,
                 name="contacts-detail",
             ),
             Route(
                 path="contacts/<int:id>/edit",
-                view=contacts.Edit.as_view(),
+                view=contacts.edit,
                 name="contacts-edit",
             ),
             Route(
                 path="contacts/<int:id>/delete",
-                view=contacts.Delete.as_view(),
+                view=contacts.delete,
                 name="contacts-delete",
             ),
-            Route(
-                path="spam/<int:spam_id>/",
-                layout=spam,
-                children=[
-                    Route(path="x/<int:eggs_id>/", view=eggs_x, name="spam-x"),
-                    Route(path="y/<int:eggs_id>/", view=eggs_y, name="spam-y"),
-                ],
-            ),
         ],
-    ).path()
+    ).path(),
 ]
